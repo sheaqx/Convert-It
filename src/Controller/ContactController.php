77f7cb7c88d580fe4aed79a06fc23c040a7017f8
Braconnier <caddaric16@gmail.com>
2022-11-23 18:26:11 +0100

    testing for contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact_index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
         return $this->render('pages/contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
---
+++ src/Form/ContactType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class)
+            ->add('email', TextType::class)
+            ->add('subject', TextType::class)
+            ->add('message', TextType::class);
+    }
+}
