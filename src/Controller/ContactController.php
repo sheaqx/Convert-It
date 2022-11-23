57aab40a2ff5005a041763e5891f1b7a35764166
Braconnier <caddaric16@gmail.com>
2022-11-23 20:49:07 +0100

    contactForm ready to style

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,18 +6,60 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact_index')]
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $html = "
+            <div style='height:100%;width:100%; background-color:#dfceba;font-family:sans-serif; text-align:center;'>
+                <div style='background-color:#8EBEE9;padding:40px;'>
+                    <h4>
+                    Exp. " . $form->getViewData()['pseudo'] . ".
+                    </h4>
+                    <h5>email: " . $form->getViewData()['email'] . " :
+                    </h5>
+                </div>
+                <div>
+                    <h4>
+                    Message :
+                    </h4>
+                    <p>" . $form->getViewData()['message'] . "
+                    </p>
+                </div>
+            </div>";
+            $email = (new Email())
+                ->from($form->getViewData()['email'])
+                ->to($this->getParameter('mailer_from'))
+                ->subject($form->getViewData()['subject'])
+                ->html($html);
+
+            $mailer->send($email);
+            // TODO Optionally:
+            // if ("message envoyé") {
+            //     echo '<script type="text/javascript">
+            //              window.onload = function () {
+            //              if(confirm("Votre message a bien été envoyé. Vous allez être redirigé vers l\'accueil.")){
+            //              window.location.href = "http://localhost:8000"
+            //              }
+            //             }
+            //             </script>';
+            // } else {
+            //     echo '<script type="text/javascript">
+            //     window.onload = function () { alert("Nous ne sommes pas parvenu a envoyer votre message"); }
+            //    </script>';
+            // };
+        }
 
         return $this->render('pages/contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'controller_name' => 'ContactController', 'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,8 +3,12 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactType extends AbstractType
 {
@@ -12,8 +16,16 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('pseudo', TextType::class)
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('subject', TextType::class)
-            ->add('message', TextType::class);
+            ->add('message', TextareaType::class)
+            ->add('send', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
     }
 }
