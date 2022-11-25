259be535b2810ec33784aeddae60b58f38b7a6f7
t-aguirre <tala.aguirre@gmail.com>
2022-11-25 14:35:26 +0100

    gallery pagination review 2

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GalleryController extends AbstractController
 {
-    #[Route('/gallery', name: 'gallery_index')]
-    public function index(): Response
-    {
+    #[Route('/gallery', name: 'gallery_index', methods: ['GET'])]
+
+    //Pagination
+    public function index(
+        PictureRepository $pictureRepository,
+        Request $request
+    ): Response {
+        $page = (int) $request->query->get('page', "1");
+        $limit = 12;
+        $pagesCount = ceil(count($pictureRepository->findAll()) / $limit);
+        $pages = range(1, $pagesCount);
+        $data = $pictureRepository->findBy([], [], $limit, ($limit * ($page - 1)));
+        // dd($data, $pages, $page, $pagesCount);
         return $this->render('pages/gallery/index.html.twig', [
-            'controller_name' => 'GalleryController', 'images' => []
+            'pictures' => $data, 'pages' => $pages
         ]);
     }
 }
