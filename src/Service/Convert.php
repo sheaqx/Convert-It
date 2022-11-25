0d5e116152c45230aa9f5f2e32033ea9eed0d266
sheaqx <111645897+sheaqx@users.noreply.github.com>
2022-11-25 15:37:44 +0100

    commit fork

---
+++ src/Service/Convert.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+class Convert
+{
+    public function Convert()
+    {
+    }
+    public function ConvertToJpg()
+    {
+    }
+    public function ConvertToPng()
+    {
+    }
+    public function ConvertToWebp()
+    {
+    }
+}
