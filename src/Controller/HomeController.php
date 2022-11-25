95b6adee967e3fc16160a79e5d457ed2f383ea23
sheaqx <sheimanelabbar@gmail.com>
2022-11-25 13:42:53 +0100

    Upload added in services instead of controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,42 +4,32 @@ namespace App\Controller;
 
 use App\Entity\Picture;
 use App\Form\UploadType;
+use App\Service\Upload;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_index')]
-    public function index(EntityManagerInterface $manager, Request $request, SluggerInterface $slugger): Response
+    public function index(EntityManagerInterface $manager, Request $request, Upload $imageUpload): Response
     {
         $upload = new Picture();
         $uploadForm = $this->createForm(UploadType::class, $upload);
         $uploadForm->handleRequest($request);
         if ($uploadForm->isSubmitted() && $uploadForm->isValid()) {
             $uploadedFile = $uploadForm->get('name')->getData();
-            $destination = $this->getParameter('kernel.project_dir') . '/public/files/temp';
-            $originaleFileName = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-            //change image name
-            $safeFileName = $slugger->slug($originaleFileName);
-            $newFileName = $safeFileName . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
-            //send image to temp folder
-            $uploadedFile->move(
-                $destination,
-                $newFileName
-            );
+            $uploadFileName = $imageUpload->imageUpload($uploadedFile);
             //Sending form info to database
-            $upload->setName($newFileName);
-            $upload->setTag($upload->getTag());
-            $upload->setDescription($upload->getDescription());
-            $upload->setSlug($upload->getName());
-            $upload->setUser($upload->getUser());
+            $upload->setName($uploadFileName)
+                ->setTag($upload->getTag())
+                ->setDescription($upload->getDescription())
+                ->setSlug($upload->getName())
+                ->setUser($upload->getUser());
             $manager->persist($upload);
             $manager->flush();
-            dd($upload);
         }
 
         return $this->renderForm('pages/home/index.html.twig', ['loggedUser' => [""], '', 'form' => $uploadForm]);
---
+++ src/Service/Upload.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class Upload
+{
+    private string $uploadDestination;
+
+    public function __construct(string $uploadDestination, private SluggerInterface $slugger)
+    {
+        $this->uploadDestination = $uploadDestination;
+        $this->slugger = $slugger;
+    }
+
+    public function imageUpload(UploadedFile $uploadedFile): string
+    {
+        $originaleFileName = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFileName = $this->slugger->slug($originaleFileName);
+        $newFileName = $safeFileName . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+        //send image to temp folder
+        $uploadedFile->move(
+            $this->getUploadDestination(),
+            $newFileName
+        );
+        return $newFileName;
+    }
+    public function getUploadDestination(): string
+    {
+        return $this->uploadDestination;
+    }
+}
