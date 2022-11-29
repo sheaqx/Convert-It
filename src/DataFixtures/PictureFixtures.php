d195d840268da6e3ac7a6b2a3dfd7b26152b240e
Braconnier <caddaric16@gmail.com>
2022-11-29 17:48:40 +0100

    fixed pictures Fixtures

--- src/DataFixtures/PictureFixtures.php
+++ src/DataFixtures/PictureFixtures.php
@@ -25,38 +25,18 @@ class PictureFixtures extends Fixture implements DependentFixtureInterface
         'moderne',
         'création'
     ];
-    public const PICTURES_NAME = [
-        "files/pictures/allGood.png",
-        "files/pictures/avion.png",
-        "files/pictures/campagne.jpg",
-        "files/pictures/canard.jpg",
-        "files/pictures/cuisine.jpg",
-        "files/pictures/foret.jpg",
-        "files/pictures/fun.jpg",
-        "files/pictures/lac.webp",
-        "files/pictures/lapin.webp",
-        "files/pictures/maison.png",
-        "files/pictures/montagne.jpg",
-        "files/pictures/moto.png",
-        "files/pictures/neige.webp",
-        "files/pictures/pluie.png",
-        "files/pictures/television.png",
-        "files/pictures/tomates.jpg",
-        "files/pictures/velo.webp",
-        "files/pictures/voiture.webp"
-    ];
-
 
     public function load(ObjectManager $manager): void
     {
-
+        $fileList = glob('public/files/pictures/*');
+        // dd($fileList);
         $faker = Factory::create('fr_FR');
         for ($i = 1; $i <= self::TOTAL_USERS; $i++) {
             for ($j = 0; $j < 5; $j++) {
                 $picture = new Picture();
                 $picture->setDescription($faker->paragraph(1, true))
                     ->setTag($faker->randomElement(self::TAGS))
-                    ->setSlug($faker->randomElement(str_replace('files/pictures/', '', self::PICTURES_NAME)))
+                    ->setSlug($faker->randomElement(str_replace('public/files/pictures/', '', $fileList)))
                     ->setName($picture->getSlug())
                     ->setUser($this->getReference(UserFixtures::PREFIX . $i));
                 $manager->persist($picture);
