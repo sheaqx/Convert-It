d081dcdb713712752e904e0c1148276cec537788
Braconnier <caddaric16@gmail.com>
2022-11-30 17:05:30 +0100

    users_index + pagination

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -5,18 +5,38 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
     #[Route('/users', name: 'users_index')]
-    public function index(UserRepository $users): Response
+    public function index(UserRepository $users, Request $request): Response
     {
+        $page = (int) $request->query->get('page', '1');
+        $limit = 4;
+        $pagesCount = ceil(count($users->findAll()) / $limit);
+        $pages = range(1, $pagesCount);
+        $prevPages = range($page - 2, $page);
+        $nextPages = range($page + 1, $page + 2);
+        $data = $users->findBy([], [], $limit, ($limit * ($page - 1)));
+
+
         return $this->render('pages/users/index.html.twig', [
-            'users' => $users->findAll(),
+            'users' => $data,
+            'prevPages' => $prevPages,
+            'nextPages' => $nextPages,
+            'pages' => $pages,
+            'page' => $page,
+            'latest' => (int) $pagesCount,
+            'pathRedirection' => 'users_index'
         ]);
     }
+    //     return $this->render('pages/users/index.html.twig', [
+    //         'users' => $users->findAll(),
+    //     ]);
+    // }
 
     #[Route('/users/show/{id}', name: 'users_show')]
     public function show(int $id, UserRepository $users): Response
